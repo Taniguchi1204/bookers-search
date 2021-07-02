bec518b6edef3e7ae771720298fd72a86ad401ba
EC2 Default User <ec2-user@ip-172-31-43-166.ec2.internal>
2021-07-02 01:53:38 +0000

    update search_method

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -4,16 +4,24 @@ class SearchsController < ApplicationController
     search_area = params[:search_area]
     search_method = params[:search_method]
     if search_area == "users"
-      if search_method == "all"
+      if search_method == "all" then
         @users = User.where('name = ?', "#{params[:keyword]}")
+      elsif search_method == "parts" then
+        @users = User.where('name like ?', "%#{params[:keyword]}%")
+      elsif search_method == "previous_parts" then
+        @users = User.where('name like ?', "#{params[:keyword]}%")
       else
-        @users = User.where('name like?', "%#{params[:keyword]}%")
+        @users = User.where('name like ?', "%#{params[:keyword]}")
       end
     else
-      if search_method == "all"
-        @books = Book.where('title = ?', "#{params[:keyword]}")
+      if search_method == "all" then
+        @books = Book.where('name = ?', "#{params[:keyword]}")
+      elsif search_method == "parts" then
+        @books = Book.where('name like ?', "%#{params[:keyword]}%")
+      elsif search_method == "previous_parts" then
+        @books = Book.where('name like ?', "#{params[:keyword]}%")
       else
-        @books = Book.where('title like?', "%#{params[:keyword]}%")
+        @books = Book.where('name like ?', "%#{params[:keyword]}")
       end
       render "search"
     end
